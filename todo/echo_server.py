04fa3faa3440d08238019a95d5d6c15259ded89a
Matthew Kenny <matthewkenny@gmail.com>
2012-02-13 22:21:47 -0800

    added DELETE

--- todo/echo_server.py
+++ todo/echo_server.py
@@ -24,28 +24,53 @@ def parse_request(sock):
     method, uri, protocol = line.split()
     return [method, uri, protocol]
 
+def clean_uri(uri):
+    if "/" in uri:
+        slash, value = uri.split("/")
+    return value
+
+def handle_request(method):
+    if method == "GET":
+        todo.load_todo_list()
+        if len(uri)==1: # Root URL Parsed
+            output = sock.send(todo.run_command('show',{'which':"All"}) + "\n")
+        elif len(uri)>1: # Specific todo given
+            value = clean_uri(uri)
+            output = sock.send(todo.run_command('show',{'which':value}) + "\n")
+
+    if method == "DELETE":
+        value = clean_uri(uri) # Don't check len(), delete todo mod will catch
+        output = sock.send(todo.run_command('delete',{'which':value}) +
+        "\n")
+
+    if method == "POST":
+        pass
+
+    if method == "PUT":
+        pass
+
+    return output
+
 if __name__ == '__main__':
     server = server_socket(host, int(port))
     print 'starting %s on %s...' % (host, port)
     try:
         while True:
             sock, client_address = server.accept()
-            request = parse_request(sock)
-            sock.send("Successfully Connected!\r\n\r\n")
 
+            todo.load_todo_list()
+            sock.send("Todo list loaded.\r\n\r\n")
+
+            request = parse_request(sock)
             uri = request[1]
             method = request[0]
             protocol = request[2]
-
             sock.send("URI: '%s' \nMethod: '%s'\r\n\r\n" % (uri, method))
 
-            if len(uri)==1: # Root URL Parsed
-                todo.load_todo_list()
-                sock.send(todo.run_command('show',{'which':"All"}) + "\n")
-            elif len(uri)>1: # Specific todo given
-                if "/" in uri:
-                    slash, value = uri.split("/")
-                    sock.send(todo.run_command('show',{'which':value}) + "\n")
+            handle_request(method)
+
+            todo.save_todo_list()
+            sock.send("Todo list saved.\r\n\r\n")
 
             sock.close()
     except KeyboardInterrupt:
