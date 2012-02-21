ee75bf90f955ed9f0a3e2e3e6692efcb12176ec3
Matthew Kenny <matthewkenny@gmail.com>
2012-02-20 21:30:29 -0800

    kml update

--- kml/update_kml.py
+++ kml/update_kml.py
@@ -4,22 +4,27 @@ value of comment field.
 """
 
 from xml.etree import ElementTree
-import os
-import pdb
-import string
 from BeautifulSoup import BeautifulSoup
+import os, string, pdb
 
-dataset = ElementTree.parse('/home/matt/Desktop/wwtp_testpit.kml') 
+namespace = 'http://www.opengis.net/kml/2.2'
+dataset = ElementTree.parse('./wwtp_testpit.kml') 
+outFile = open('./wwtp_testpit_out.kml', "wb")
 root = dataset.getroot()
-placemarks = root.findall('{http://www.opengis.net/kml/2.2}Document/{http://www.opengis.net/kml/2.2}Folder/{http://www.opengis.net/kml/2.2}Placemark')
+placemarks = root.findall('{%s}Document/{%s}Folder/{%s}Placemark' % (namespace, namespace,
+    namespace))
 
 for placemark in placemarks:
-    elemName = placemark.find('{http://www.opengis.net/kml/2.2}name')
+    elemName = placemark.find('{%s}name' % namespace)
     print 'Original Name: ' + elemName.text 
-    description = placemark.find('{http://www.opengis.net/kml/2.2}description').text
-    htmlDesc = BeautifulSoup(description)
+    elemDesc = placemark.find('{%s}description' % namespace).text
+    htmlDesc = BeautifulSoup(elemDesc)
     tables = htmlDesc.findAll('td')
-    realName = tables[1].text
-    elemName.__setattr__('text',realName)
-    print 'Comment Value: ' + realName
-    print 'Updated Name: ' + placemark.find('{http://www.opengis.net/kml/2.2}name').text
+    newName = tables[1].text
+    elemName.text = newName
+    print 'Comment Value: ' + newName
+    print 'Updated Name: ' + placemark.find('{%s}name' % namespace).text
+
+dataset.write(outFile)
+outFile.close()
+pdb.set_trace()
